d412d2e79b6354a7fd913c507b26b91729a07ed7
headlessNode <harriskhan047@outlook.com>
2024-10-20 19:29:08 +0500

    docs: add JsDocs to query functions

--- db/query.js
+++ db/query.js
@@ -1,21 +1,44 @@
 require('dotenv').config();
 const pool = require('./pool.js');
 
+/**
+ * Retrieves all games from the database.
+ *
+ * @return {Promise<Object[]>} The result of the query as an array of objects, where each object represents a game.
+ */
 async function getAllGames(){
     const {rows} = await pool.query('SELECT * FROM games');
     return rows;
 }
 
+/**
+ * Retrieves all distinct developers from the database.
+ *
+ * @return {Promise<Object[]>} The result of the query as an array of objects, where each object has a 'developer' property.
+ */
 async function getAllDevelopers(){
     const {rows} = await pool.query('SELECT DISTINCT developer FROM developers ORDER BY developer ASC');
     return rows;
 }
 
+
+/**
+ * Retrieves all distinct genres from the database.
+ *
+ * @return {Promise<Object[]>} The result of the query as an array of objects, where each object has a 'genre' property.
+ */
 async function getAllGenre(){
     const {rows} = await pool.query('SELECT DISTINCT genre FROM genre ORDER BY genre ASC');
     return rows;
 }
 
+/**
+ * Filters games by developer and genre.
+ *
+ * @param {String} developer The developer to filter by.
+ * @param {String} genre The genre to filter by.
+ * @return {Promise<Object[]>|String} The result of the query.
+ */
 async function filterGames(developer, genre){
     if(developer === 'All' && genre === 'All'){
         return 'All';
@@ -45,6 +68,14 @@ async function filterGames(developer, genre){
     }
 }
 
+/**
+ * Updates the title and img_url of a game with the given id.
+ *
+ * @param {Number} id The id of the game to update.
+ * @param {String} title The new title of the game.
+ * @param {String} img_url The new url of the game.
+ * @return {Promise<void>} A promise that resolves when the update has been completed.
+ */
 async function updateGame(id, title, img_url){
     await pool.query(
         `UPDATE games
